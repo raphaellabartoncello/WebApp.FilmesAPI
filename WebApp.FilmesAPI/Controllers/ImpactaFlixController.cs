e809d19e77029f2c0d0f116c6a079d4849df5bde
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-25 20:14:49 -0300

    Construir a controller

---
+++ WebApp.FilmesAPI/Controllers/ImpactaFlixController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.FilmesAPI.Controllers
+{
+    public class ImpactaFlixController : Controller
+    {
+        // GET: ImpactaFlix
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
