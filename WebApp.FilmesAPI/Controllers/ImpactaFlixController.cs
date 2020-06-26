ebe8e7f7a572c81d10921f7f5a976b5710ec43c2
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-25 22:11:08 -0300

    Criar método listar filme e ajustar a consulta por parametro na Index

--- WebApp.FilmesAPI/Controllers/ImpactaFlixController.cs
+++ WebApp.FilmesAPI/Controllers/ImpactaFlixController.cs
@@ -26,7 +26,11 @@ namespace WebApp.FilmesAPI.Controllers
                 //Definir a origem
                 client.BaseAddress = new Uri("https://api.themoviedb.org/3/");
 
-                var response = client.GetAsync("movie/550?api_key=f5c535362407d4358c0d43c3a2fe8b8b");
+                //Receber por parametro
+                string apiKey = "f5c535362407d4358c0d43c3a2fe8b8b";
+                int codigoFilme = 550;
+                var response = client.GetAsync(string.Format("movie/{0}?api_key={1}", codigoFilme, apiKey));
+
                 //Esperar a resposta do site (aguardar)
                 response.Wait();
 
@@ -54,6 +58,63 @@ namespace WebApp.FilmesAPI.Controllers
 
                 //Devolver o resultado da consulta para a VIEW
                 return View(filme);
+            }
+        }
+
+        public ActionResult Lista()
+        {
+            //Criar uma lista 
+            List<FilmeMOD> filmesLista = null;
+
+            //Variável que obterá o filme
+            FilmeMOD filme = null;
+
+            //Vamos criar um using para criar o nosso objeto HttpClient:
+            //Objeto que permite a navegação, usar o protocolo Http, fazer teste de páginas, etc...
+            //Permite navegar como se fosse o browser via back end
+            using (var client = new HttpClient())
+            {
+                //http get no site de filmes
+                //Definir a origem
+                client.BaseAddress = new Uri("https://api.themoviedb.org/3/");
+
+                //Receber por parametro
+                string apiKey = "f5c535362407d4358c0d43c3a2fe8b8b";
+                int codigoFilme = 550;
+                var response = client.GetAsync(string.Format("movie/{0}?api_key={1}", codigoFilme, apiKey));
+                
+                //Esperar a resposta do site (aguardar)
+                response.Wait();
+
+                //Pegar o resultado
+                var result = response.Result;
+
+                //Verificamos se o código retornou com sucesso
+                if (result.IsSuccessStatusCode)
+                {
+                    //Receber o retorno do JSON de filmes que é convertido para a classe FilmeMOD automaticamente pelo .net
+                    filme = result.Content.ReadAsAsync<FilmeMOD>().Result;
+
+                    filmesLista = new List<FilmeMOD>();
+
+                    filmesLista.Add(filme);
+
+                    //SE O JSON RETORNASSE UM ARRAY EU PRECISARIA RECEBER COM UM ARRAY
+                    //var resultadoGET = result.Content.ReadAsAsync<IList<FilmeMOD>>();
+                    //resultadoGET.Wait();
+                    //filmes = resultadoGET.Result;
+                }
+
+                else
+                {
+                    //se não tiver sucesso devolver um model state com erro tratado - mensagem personalizada
+                    filmesLista = new List<FilmeMOD>();
+
+                    ModelState.AddModelError(string.Empty, "Erro ao tentar consultar dados no servidor de filmes");
+                }
+
+                //Devolver o resultado da consulta para a VIEW
+                return View(filmesLista);
             }
         }
     }
